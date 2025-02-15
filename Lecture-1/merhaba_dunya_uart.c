6c28fc0417c7a52150bcfd05e562a5f875d3ef6b
skynet1881 <skynettech1992@gmail.com>
2025-02-15 15:28:08 +0100

    First lecture

---
+++ Lecture-1/merhaba_dunya_uart.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() 
+{
+    // UART1 is used by Zynq PS
+    printf("Merhaba Dunya, Ben Zybo!\n");
+    printf("Bu bizim ilk dersimiz, hosgeldiniz!\n");
+
+    return 0;
+}
+
